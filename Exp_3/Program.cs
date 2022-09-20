efa99e582602a8dc070004d6f4895092dbb0f2a4
Feonorka <104940479+Feonorka@users.noreply.github.com>
2022-09-20 22:10:42 +0300

    Добавлен генератор случайных чисел

--- Exp_3/Program.cs
+++ Exp_3/Program.cs
@@ -1,4 +1,6 @@
-﻿int NumberA = 23;
-int NumberB = 5;
+﻿int NumberA = new Random().Next(1, 10); // Используется случайное число от 1 до 9
+Console.WriteLine(NumberA);
+int NumberB = new Random().Next(1, 10);
+Console.WriteLine(NumberB);
 int res = NumberA + NumberB;
 Console.WriteLine(res);
