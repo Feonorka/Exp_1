d6b8706ab80e1300a3d4fdb2846b0b15a260fc90
Feonorka <104940479+Feonorka@users.noreply.github.com>
2022-09-20 22:27:48 +0300

    Работа со строками

---
+++ Exp_5/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "саша")
+{
+    Console.WriteLine("Ура! Это ты?");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
